9d701b6f114ebc719b6374d688bfa8f0bfd46df4
Vigenere <cmmonroe2010@gmail.com>
2017-03-11 14:45:58 -0500

    Adding audio streaming response, DI still doesn't work

---
+++ YTConvert/AudioStream.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTConvert
+{
+    public class AudioStream
+    {
+        string _filelocation;
+        string _ext;
+        public AudioStream(string filename, string ext)
+        {
+            _ext = ext;
+            _filelocation = Assembly.GetExecutingAssembly().Location + @"\" + filename + "." + _ext;
+        }
+        public async void WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+        {
+            try
+            {
+                var buffer = new byte[65536];
+
+                using (var audio = File.Open(_filelocation, FileMode.Open, FileAccess.Read))
+                {
+                    var length = (int)audio.Length;
+                    var bytesRead = 1;
+
+                    while (length > 0 && bytesRead > 0)
+                    {
+                        bytesRead = audio.Read(buffer, 0, Math.Min(length, buffer.Length));
+                        await outputStream.WriteAsync(buffer, 0, bytesRead);
+                        length -= bytesRead;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            finally
+            {
+                outputStream.Close();
+            }
+        }
+    }
+}
--- YTConvert/ConvertController.cs
+++ YTConvert/ConvertController.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -26,6 +29,15 @@ namespace YTConvert
             Debug.WriteLine($"URL is {url}");
             _converter.Convert(url);
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        }
+        [Route("stream")]
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]string filename, string ext)
+        {
+            var audio = new AudioStream(filename, ext);
+            var response = Request.CreateResponse();
+            response.Content = new PushStreamContent((Action<Stream, HttpContent, TransportContext>)audio.WriteToStream, new MediaTypeHeaderValue("audio/mp4"));
+            return response;
         }
     }
 }
